2f9c6d3c3c388061a27bf7e802f8cf88eb30b916
Tobias Kunze <rixx@cutebit.de>
2016-11-12 23:59:49 +0100

    [drehregler] Save variables in redis.

--- src/drehregler.py
+++ src/drehregler.py
@@ -1,38 +1,49 @@
 import asyncio
 import sys
 
+import redis
 
-PER_SECOND = 0
-THRESHOLD = 100
+
+REDIS = None
+REDIS_RATE = 'dreh.PER_SECOND'
+REDIS_TARGET = 'MAXUSERS'
+REDIS_THRESHOLD = 'dreh.THRESHOLD'
 
 
 async def update_redis():
+    global REDIS
     while True:
-        global PER_SECOND
-        global THRESHOLD
+        rate = int(REDIS.get(REDIS_RATE) or 0)
+        threshold = int(REDIS.get(REDIS_THRESHOLD) or 0)
+        current = int(REDIS.get(REDIS_TARGET) or 0)
 
-        if PER_SECOND > 0:
-            print('Incrementing by {}. Limit is {}.'.format(PER_SECOND, THRESHOLD))
-        elif PER_SECOND == 0:
-            print('Not incrementing.')
+        if rate > 0:
+            if current + rate < threshold:
+                REDIS.incrby(REDIS_TARGET, rate)
+                print('Incrementing by {}. Current is {}. Limit is {}.'.format(rate, current, threshold))
+            else:
+                print('Not incrementing, would cross threshold.')
+        elif rate == 0:
+            print('Not incrementing, rate is 0.')
         await asyncio.sleep(1)
 
 
 def get_user_input():
-    global PER_SECOND
-    global THRESHOLD
+    global REDIS
 
     command = sys.stdin.readline().strip()
     if command.startswith('t '):
         try:
-            THRESHOLD = int(command[2:])
-            print('Moved threshold to {}'.format(THRESHOLD))
+            threshold = int(command[2:])
+            REDIS.set(REDIS_THRESHOLD, threshold)
+            print('Moved threshold to {}'.format(threshold))
         except:
             print('Could not parse threshold value.')
     elif command.startswith('r '):
         try:
-            PER_SECOND = int(command[2:])
-            print('Moved rate to {}'.format(PER_SECOND))
+            rate = int(command[2:])
+            REDIS.set(REDIS_RATE, rate)
+            print('Moved rate to {}'.format(rate))
         except Exception as e:
             print('Could not parse rate value "{}": {}'.format(command[2:], str(e)))
     else:
@@ -40,10 +51,13 @@ def get_user_input():
 
 
 if __name__ == '__main__':
-    print('Starting with {}'.format(PER_SECOND))
-    update_task = asyncio.async(update_redis())
+    REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
+    REDIS.set(REDIS_RATE, 0)
+    REDIS.set(REDIS_THRESHOLD, 100)
+
     loop = asyncio.get_event_loop()
     loop.add_reader(sys.stdin, get_user_input)
+    update_task = asyncio.async(update_redis())
 
     try:
         loop.run_forever()
